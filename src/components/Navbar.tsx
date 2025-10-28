6a63a73a4262bf942dc256c70a04eef3684d3105
MxAziz <hereabdulaziz@gmail.com>
2025-10-28 16:43:26 +0600

    Handling Active Links in Next.js using usePathName() hook

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,51 @@
 'use client';
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathName = usePathname();
+  console.log(pathName);
+
+
   return (
     <div className="py-4 px-8 bg-gray-800 flex justify-between items-center">
-          <Link
-            href="/"
-            className="text-white text-lg font-semibold"
-          >
-            Next.js App
-          </Link>
-          <nav className="flex gap-4">
-              <Link href="/products">Products</Link>
-              <Link href="/about">About</Link>
-              <Link href="/contact">Contact</Link>
-              <Link href="/blog/1">Blog</Link>
-          </nav>
+      <Link href="/" className="text-white text-lg font-semibold">
+        Next.js App
+      </Link>
+      <nav className="flex gap-4">
+        <Link
+          href="/products"
+          className={`${
+            pathName === "/products" ? "text-yellow-300" : "text-white"
+          }`}
+        >
+          Products
+        </Link>
+        <Link
+          href="/about"
+          className={`${
+            pathName === "/about" ? "text-yellow-300" : "text-white"
+          }`}
+        >
+          About
+        </Link>
+        <Link
+          href="/contact"
+          className={`${
+            pathName === "/contact" ? "text-yellow-300" : "text-white"
+          }`}
+        >
+          Contact
+        </Link>
+        <Link
+          href="/blog/1"
+          className={`${
+            pathName === "/blog/1" ? "text-yellow-300" : "text-white"
+          }`}
+        >
+          Blog
+        </Link>
+      </nav>
     </div>
   );
 }
